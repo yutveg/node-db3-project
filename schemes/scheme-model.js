bf2ce4a32948e6d13294ff2a5c4ea2eff2cc0628
yutveg <kodi.0n31l@gmail.com>
2020-01-22 14:31:35 -0800

    helpers almost finished working on last return and then steps

---
+++ schemes/scheme-model.js
@@ -0,0 +1,51 @@
+const db = require("../data/dbconfig.js");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db("schemes").select("*");
+}
+
+function findById(id) {
+  return db("schemes")
+    .select("*")
+    .where({ id })
+    .first();
+}
+
+function findSteps(id) {
+  return db("schemes").join("steps", "steps.scheme_id", "schemes.id");
+}
+
+function add(scheme) {
+  return db("schemes")
+    .insert(scheme)
+    .then(([id]) => {
+      return findById(id);
+    });
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes, id)
+    .then(res => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return db("schemes")
+    .del()
+    .where({ id })
+    .then(res => {
+      return findById(id);
+    });
+}
